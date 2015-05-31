c245ef83dd703045256c650c9206b78f7282edad
Erik Nomitch <enomitch@gmail.com>
2015-05-31 00:31:48 -0500

    + Pusher test send

--- lib/notify-push.rb
+++ lib/notify-push.rb
@@ -1,3 +1,14 @@
 require "dotenv"
+require "pusher"
 
 Dotenv.load
+
+module NotifyPush
+  def self.start_sender()
+    Pusher.url = "http://#{ENV["PUSHER_KEY"]}:#{ENV["PUSHER_SECRET"]}@api.pusherapp.com/apps/#{ENV["PUSHER_APP_ID"]}"
+
+    Pusher['test_channel'].trigger('my_event', {
+      message: 'hello world'
+    })
+  end
+end
