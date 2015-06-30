5c58feedaff9f395e7883c6d58e573d1b5171e9f
Erik Nomitch <enomitch@gmail.com>
2015-06-30 12:16:25 -0500

    adds more descriptions to commands

--- lib/notify-push/cli.rb
+++ lib/notify-push/cli.rb
@@ -16,7 +16,7 @@ module NotifyPush
     # --------------------------------------------
     # COMMAND->RECEIVE ---------------------------
     # --------------------------------------------
-    desc "receive", "Starts the Receiver daemon."
+    desc "receive", "Starts the Receiver server (in the foreground)."
     option :silence_events, type: :boolean, default: false
     def receive()
       NotifyPush.main :receive
@@ -25,7 +25,7 @@ module NotifyPush
     # --------------------------------------------
     # COMMAND->SEND ------------------------------
     # --------------------------------------------
-    desc "send MESSAGE <TITLE>", "say receive to NAME"
+    desc "send MESSAGE <TITLE>", "Sends data to any listening Receivers."
     def send(message, title="notify-push")
 
       # FIX: Use these arguments instead of ARGV
@@ -35,14 +35,17 @@ module NotifyPush
     # --------------------------------------------
     # COMMAND->INSTALL ---------------------------
     # --------------------------------------------
-    desc "install COMPONENT", "Installs a notify-push component. Available COMPONENTs: receiver-daemon"
+    desc "install COMPONENT", "Installs a notify-push component. Available COMPONENT(s): receiver-daemon"
     def install(component)
       puts "installing..."
     end
     
-    desc "uninstall COMPONENT", "say receive to NAME"
+    # --------------------------------------------
+    # COMMAND->UNINSTALL -------------------------
+    # --------------------------------------------
+    desc "uninstall COMPONENT", "Uninstalls a notify-push component."
     def uninstall(component)
-      puts "installing..."
+      puts "uninstalling..."
     end
   end
 
